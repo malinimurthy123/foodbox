87f7f9848dec8fa822449da9d4d49e8014a96591
MaliniMurthy <MMurthy@autoeveramerica.com>
2023-04-10 22:25:13 -0700

    UI Designing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { environment} from './../environments/environment';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule
