113b4f3cf229570594e717bb1d694650e77ebd87
MaliniMurthy <MMurthy@autoeveramerica.com>
2023-04-10 23:00:17 -0700

    Admin UI Designing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,14 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { environment} from './../environments/environment';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { AdminLoginComponent } from './admin/admin-login/admin-login.component';
-
+import { AdminPageComponent } from './admin/admin-page/admin-page.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 
 @NgModule({
   declarations: [
